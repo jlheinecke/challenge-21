2c88f70ed97a02601b89321116e30debf218a43b
jimh <james.l.heinecke@gmail.com>
2023-08-12 18:10:15 -0500

    debugging SearchBooks page

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import SearchBooks from './pages/SearchBooks';
-// import SavedBooks from './pages/SavedBooks';
-// import Navbar from './components/Navbar';
-
-// function App() {
-//   return (
-//     <Router>
-//       <>
-//         <Navbar />
-//         <Routes>
-//           <Route 
-//             path='/' 
-//             element={<SearchBooks />} 
-//           />
-//           <Route 
-//             path='/saved' 
-//             element={<SavedBooks />} 
-//           />
-//           <Route 
-//             path='*'
-//             element={<h1 className='display-2'>Wrong page!</h1>}
-//           />
-//         </Routes>
-//       </>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -49,14 +15,16 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <>
+        
           <Navbar />
           <Routes>
+            
             <Route path='/' element={<SearchBooks />} />
             <Route path='/saved' element={<SavedBooks />} />
             <Route path='*' element={<h1 className='display-2'>Wrong page!</h1>} />
+            
           </Routes>
-        </>
+        
       </Router>
     </ApolloProvider>
   );
--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: false,
 });
 
 module.exports = mongoose.connection;
